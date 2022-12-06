a07ed3d9afb281c52ec85cc37ed572f5a9f8993a
imcvampire <quocanh96@gmail.com>
2022-12-06 22:33:12 +0200

    feat(*): day2b

--- 2022/day2b/src/main.rs
+++ 2022/day2b/src/main.rs
@@ -12,14 +12,36 @@ impl TryFrom<char> for Move {
 
     fn try_from(c: char) -> Result<Self, Self::Error> {
         match c {
-            'A' | 'X' => Ok(Move::Rock),
-            'B' | 'Y' => Ok(Move::Paper),
-            'C' | 'Z' => Ok(Move::Scissors),
+            'A' => Ok(Move::Rock),
+            'B' => Ok(Move::Paper),
+            'C' => Ok(Move::Scissors),
             _ => Err(color_eyre::eyre::eyre!("not a valid move: {c:?}")),
         }
     }
 }
 impl Move {
+    const ALL_MOVES: [Move; 3] = [Move::Rock, Move::Paper, Move::Scissors];
+
+    fn winning_move(self) -> Self {
+        Self::ALL_MOVES
+            .iter()
+            .copied()
+            .find(|m| m.beats(self))
+            .expect("at least one move beats us")
+    }
+
+    fn losing_move(self) -> Self {
+        Self::ALL_MOVES
+            .iter()
+            .copied()
+            .find(|&m| self.beats(m))
+            .expect("at least one move beats us")
+    }
+
+    fn drawing_move(self) -> Self {
+        self
+    }
+
     fn inherent_points(self) -> usize {
         match self {
             Move::Rock => 1,
@@ -50,11 +72,30 @@ impl Move {
 
 #[derive(Debug, Clone, Copy)]
 enum Outcome {
-    Win,
-    Draw,
     Loss,
+    Draw,
+    Win,
+}
+impl TryFrom<char> for Outcome {
+    type Error = color_eyre::Report;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            'X' => Ok(Outcome::Loss),
+            'Y' => Ok(Outcome::Draw),
+            'Z' => Ok(Outcome::Win),
+            _ => Err(color_eyre::eyre::eyre!("not a valid outcome: {c:?}")),
+        }
+    }
 }
 impl Outcome {
+    fn matching_move(self, theirs: Move) -> Move {
+        match self {
+            Outcome::Win => theirs.winning_move(),
+            Outcome::Draw => theirs.drawing_move(),
+            Outcome::Loss => theirs.losing_move(),
+        }
+    }
     fn inherent_points(self) -> usize {
         match self {
             Outcome::Win => 6,
@@ -74,14 +115,14 @@ impl FromStr for Round {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut chars = s.chars();
-        let (Some(theirs), Some(' '), Some(ours), None) = (chars.next(), chars.next(), chars.next(), chars.next()) else {
-      return Err(color_eyre::eyre::eyre!("expected <theirs>SP<ours>EOF, got {s:?}"));
-    };
+        let (Some(theirs), Some(' '), Some(outcome), None) = (chars.next(), chars.next(), chars.next(), chars.next()) else {
+            return Err(color_eyre::eyre::eyre!("expected <theirs>SP<outcome>EOF, got {s:?}"));
+        };
+        let theirs = Move::try_from(theirs)?;
+        let outcome = Outcome::try_from(outcome)?;
+        let ours = outcome.matching_move(theirs);
 
-        Ok(Self {
-            theirs: theirs.try_into()?,
-            ours: ours.try_into()?,
-        })
+        Ok(Self { theirs, ours })
     }
 }
 impl Round {
@@ -97,7 +138,7 @@ impl Round {
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
-    let total_score: usize = process_results(
+    let total_score: usize = itertools::process_results(
         include_str!("input.txt")
             .lines()
             .map(Round::from_str)
