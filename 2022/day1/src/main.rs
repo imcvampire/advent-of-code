d4a3f627268fb0e5a7666cf219a81aa54acd7f1d
imcvampire <quocanh96@gmail.com>
2022-12-06 02:34:21 +0200

    feat(*): day1

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -1,17 +1,22 @@
+use itertools::Itertools;
+
 fn main() -> color_eyre::Result<()> {
   color_eyre::install()?;
 
-  let lines = include_str!("input.txt")
+  let answer = include_str!("input.txt")
     .lines()
     .map(|v| v.parse::<u64>().ok())
-    .collect::<Vec<_>>();
-
-  let elven_lead = lines
-    .split(|line| line.is_none())
-    .map(|group| group.iter().map(|v| v.unwrap()).sum::<u64>())
-    .max();
-
-  println!("{elven_lead:?}");
+    .coalesce(|a, b| match (a, b) {
+      (None, None) => Ok(None),
+      (None, Some(b)) => Ok(Some(b)),
+      (Some(a), Some(b)) => Ok(Some(a + b)),
+      (Some(a), None) => Err((Some(a), None)),
+    })
+    .flatten()
+    .sorted_by_key(|&v| u64::MAX - v)
+    .take(3)
+    .sum::<u64>();
+  println!("{answer:?}");
 
   Ok(())
 }
