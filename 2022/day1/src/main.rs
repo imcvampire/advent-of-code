563ab67ce9d393abed2d74270884384f1eaeb6c2
imcvampire <quocanh96@gmail.com>
2022-12-06 13:02:48 +0200

    feat(*): day2a

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -1,22 +1,22 @@
 use itertools::Itertools;
 
 fn main() -> color_eyre::Result<()> {
-  color_eyre::install()?;
+    color_eyre::install()?;
 
-  let answer = include_str!("input.txt")
-    .lines()
-    .map(|v| v.parse::<u64>().ok())
-    .coalesce(|a, b| match (a, b) {
-      (None, None) => Ok(None),
-      (None, Some(b)) => Ok(Some(b)),
-      (Some(a), Some(b)) => Ok(Some(a + b)),
-      (Some(a), None) => Err((Some(a), None)),
-    })
-    .flatten()
-    .sorted_by_key(|&v| u64::MAX - v)
-    .take(3)
-    .sum::<u64>();
-  println!("{answer:?}");
+    let answer = include_str!("input.txt")
+        .lines()
+        .map(|v| v.parse::<u64>().ok())
+        .coalesce(|a, b| match (a, b) {
+            (None, None) => Ok(None),
+            (None, Some(b)) => Ok(Some(b)),
+            (Some(a), Some(b)) => Ok(Some(a + b)),
+            (Some(a), None) => Err((Some(a), None)),
+        })
+        .flatten()
+        .sorted_by_key(|&v| u64::MAX - v)
+        .take(3)
+        .sum::<u64>();
+    println!("{answer:?}");
 
-  Ok(())
+    Ok(())
 }
